fc0558cfd2f8bd97fee28f0f4090d74f0ed9d6f9
moovi <moovi@nate.com>
2018-12-03 15:01:58 +0900

    [moovi] 메뉴수정

---
+++ src/components/contents/systemMonitoring/ResourceSystem.vue
@@ -0,0 +1,22 @@
+<template>
+    
+</template>
+
+<script lang="ts">
+
+    import {Component, Vue} from "vue-property-decorator";
+
+    @Component({
+        components: {
+            name: "ResourceSystem"
+
+        }
+    })
+    export default class ResourceSystem extends Vue {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/contents/systemMonitoring/ServiceStatus.vue
@@ -0,0 +1,27 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "ServiceStatus"
+    }
+</script>
+<script lang="ts">
+
+    import {Component, Vue} from "vue-property-decorator";
+
+    @Component({
+        components: {
+            name: "ServiceStatus"
+
+        }
+    })
+    export default class ServiceStatus extends Vue {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
