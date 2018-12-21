7062195da3db811dcc7e1cd8826c7772d272feb6
moovi <moovi@nate.com>
2018-12-21 18:06:15 +0900

    [moovi] fnq

--- src/components/contents/fnq/FnqList.vue
+++ src/components/contents/fnq/FnqList.vue
@@ -14,57 +14,51 @@
             </div>
             </template >
 
+            <template v-if="role == '0001' || role == '0003'">
+                <!--리스트-->
+                <ListComponent v-bind:listObject="listItem" v-bind:onLoadList="listItem.dataGrid.onLoadList" v-on:listView="listViewEvent"></ListComponent>
+            </template>
 
+            <template v-else>
+                ========================================================================================================
+                <!--btn mid -->
+                <p class="btn_top type02">
+                    <button type="button"  class="btn_s01 bg02 manual" v-on:click="manual"><i class="icon download"></i>매뉴얼 다운로드</button>
+                </p>
+                <!-- search bbs box -->
+                <div class="search_bbs_box type01">
+                    <ul class="search_list">
+                        <li>
+                            <input type="text" class="input sch_notice" placeholder="제목+내용" title="검색어 입력" v-model="searchKey" v-on:keyup.enter="searchFaq">
+                        </li>
+                        <li>
+                            <span class="btn_area">
+                                <button type="button"  class="btn_sch01" v-on:click="searchFaq">검색</button>
+                                <img src="../../../assets/images/icon_reset.png" v-on:click="searchReset">
+                            </span>
+                        </li>
+                    </ul>
+                </div>
+                <!-- //search bbs box -->
 
-            <!--<template v-if=""> 관리자,콜센터일때-->
-
-
-            <!--리스트-->
-            <ListComponent v-bind:listObject="listItem" v-bind:onLoadList="listItem.dataGrid.onLoadList" v-on:listView="listViewEvent"></ListComponent>
-            <!-- pagination -->
-            <!--<Paging v-bind:pagingDetail=listItem.paging v-on:pageToList="pagingEvent" ></Paging>-->
-            <!-- //pagination -->
-
-            <!--</template>-->
-========================================================================================================
-            <!--<template v-else> 일반관리자-->
-            <!-- faq box -->
-
-            <!-- btn mid -->
-            <!--<p class="btn_top type02">-->
-                <!--<button type="button"  class="btn_s01 bg02 manual" v-on:click="manual"><i class="icon download"></i>매뉴얼 다운로드</button>-->
-            <!--</p>-->
-            <!--&lt;!&ndash; search bbs box &ndash;&gt;-->
-            <!--<div class="search_bbs_box type01">-->
-                <!--<ul class="search_list">-->
-                    <!--<li>-->
-                        <!--<input type="text" class="input sch_notice" placeholder="제목+내용" title="검색어 입력" v-model="searchKey" v-on:keyup.enter="searchFaq">-->
-                    <!--</li>-->
-                    <!--<li>-->
-                            <!--<span class="btn_area">-->
-                                <!--<button type="button"  class="btn_sch01" v-on:click="searchFaq">검색</button>-->
-                                <!--<img src="../../../assets/images/icon_reset.png" v-on:click="searchReset">-->
-                            <!--</span>-->
-                    <!--</li>-->
-                <!--</ul>-->
-            <!--</div>-->
-            <!--&lt;!&ndash; //search bbs box &ndash;&gt;-->
-
-            <!--<div class="faq_box">-->
-                <!--<dl class="faq_list">-->
-                    <!--<template v-if="listData.length > 0">-->
-                        <!--<template v-for="(datas, index) in listData">-->
-                            <!--<dt v-on:click=" display(index)" v-bind:class="{ 'on': (rownum == index) } " ><i class="icon faq_q">Q</i> {{datas.title}}</dt>-->
-                            <!--<dd v-show="rownum == index"><i class="icon faq_a">A</i> {{datas.content}}</dd>-->
-                        <!--</template>-->
-                    <!--</template>-->
-                    <!--<template v-else>-->
-                        <!--<dd style="text-align: center">데이터가 없습니다.</dd>-->
-                    <!--</template>-->
-                <!--</dl>-->
-            <!--</div>-->
-            <!-- //faq box -->
-            <!--</template>-->
+                <div class="faq_box">
+                    <dl class="faq_list">
+                        <template v-if="listData.length > 0">
+                            <template v-for="(datas, index) in listData">
+                                <dt v-on:click=" display(index)" v-bind:class="{ 'on': (rownum == index) } " ><i class="icon faq_q">Q</i> {{datas.title}}</dt>
+                                <dd v-show="rownum == index"><i class="icon faq_a">A</i> {{datas.content}}</dd>
+                            </template>
+                        </template>
+                        <template v-else>
+                            <dd style="text-align: center">데이터가 없습니다.</dd>
+                        </template>
+                    </dl>
+                </div>
+                <!--//faq box-->
+                <!-- pagination -->
+                <Paging v-bind:pagingDetail=pagingUser v-on:pageToList="pagingEvents" ></Paging>
+                <!-- //pagination -->
+            </template>
 
         </div>
         <!-- //content -->
@@ -84,6 +78,8 @@
     import moment from 'moment'
     Vue.prototype.moment = moment;
 
+    import {format} from 'date-fns';
+
     @Component({
         components: {
             FnqList, Paging, ListComponent
@@ -94,20 +90,36 @@
         listData: any = [];
         searchType: string = '';
         searchKey: string = '';
-        // totalCount: any = '';
-        // startPage: any = '';
-        role: any = sessionStorage.getItem('role');
+        totalCount: any = '';
+        startPage: any = '';
+        searchStartDate_str: any = '';
+        searchEndDate_str: any =  format(new Date(),'YYYYMMDD');
+
+        // role: string = sessionStorage.getItem('role');
+        role: string = '0001';
 
         originItem : any = {} // 오리지널데이터
-        listItem: any =  // 그리드 서치 페이징 옵션 처리 데이터 매우중요 이룰을 어기면 화면깨짐이 발생합니다
-            {
+        listItem: any = {} // 그리드 서치 페이징 옵션 처리 데이터 매우중요 이룰을 어기면 화면깨짐이 발생합니다
+
+
+        isActive:boolean=false;
+        arrow:string="";
+        rownum:number=999;
+        pagingUser : any =   { currentPage : 1 , lastPage : 3 ,viewPageSize : 10 ,totalRecords : 3 , from : 1 , to : 3 , perPage : 10};
+
+        created(){
+            this.searchStartDate_str =new Date();
+            this.searchStartDate_str.setFullYear(this.searchStartDate_str.getFullYear()-3);
+            this.searchStartDate_str = format(this.searchStartDate_str ,'YYYYMMDD');
+
+            this.listItem={
                 dataGrid: {
                     columControl:[  // 반드시 받는 컬럼명과 이 ID 가 같아야데이터가 나옵니다..
                         {columName : '순번', type:'number', id : 'rnum', width : '10%' , height : '' , size : '' , mobile : 'N' , cols : '' , rows : '' ,rowColors :'' },
                         {columName : '제목', type:'text', id : 'title', width : '60%' , height : '' , size : '' , mobile : 'N' , cols : '' , rows : '' ,rowColors :'' },
                         {columName : '등록일', type:'text', id : 'regDt', width : '10%' , height : '' , size : '' , mobile : 'N' , cols : '' , rows : ''},
-                        {columName : '등록자', type:'text', id : 'regRole', width : '10%' , height : '' , size : '' , mobile : 'N' , cols : '' , rows : ''},
-                        {columName : '구분', type:'text', id : 'viewType', width : '10%' , height : '' , size : '' , mobile : 'N' , cols : '' , rows : ''   }, // 라인컬러와 라인벨류는 오직하나만
+                        {columName : '등록자', type:'text', id : 'roleNm', width : '10%' , height : '' , size : '' , mobile : 'N' , cols : '' , rows : ''},
+                        {columName : '구분', type:'text', id : 'viewType', width : '10%' , height : '' , size : '' , mobile : 'N' , cols : '' , rows : ''  ,options : [{ value : 'ADM' , change : '관리자'},{ value : 'USR' , change : '사용자'}] }, // 라인컬러와 라인벨류는 오직하나만
                     ],
                     totalColum: 5,
                     apiUrl : 'faq',
@@ -115,31 +127,27 @@
                 },
                 // 아이디는 실제 컬럼값을 넣어주면됩니다.
                 search: [
-                    {type: 'date', title :'등록일', id: 'date' , name:'date', searchStartDate: null ,  searchEndDate: null, calenderCount : 2},
+                    {type: 'date', title :'등록일', id: 'date' , name:'date', searchStartDate: this.searchStartDate_str ,  searchEndDate: this.searchEndDate_str, calenderCount : 2},
                     {type: 'select' , title :'구분',id: 'importantYn', name:'importantYn' , value: '' ,  api : '' , option : [{ name : '공통' , value: 'ALL' },{ name : '사용자' , value: 'USR' },{name : '관리자' , value: 'ADM' }]},
                     {type: 'input2' , title :'검색어',id: 'searchWord', name:'searchWord' , value: '' ,placeholder:'제목+내용',  },
                 ],
                 searchClass: 'search_box page_customer04',
                 searchClass2: 'search_list col0301',
-                paging: { currentPage : 1 , lastPage : 0 ,viewPageSize : 10 ,totalRecords : 0 , from : 1 , to : 0 , perPage : 10},
+                paging: { currentPage : 1 , lastPage : 3 ,viewPageSize : 10 ,totalRecords : 3 , from : 1 , to : 3 , perPage : 10},
                 goDirect : ""
             }
 
-        isActive:boolean=false;
-        arrow:string="";
-        rownum:number=999;
 
-        created(){
             this.originItem  = this.listItem.dataGrid.columControl
         }
 
 
-
+reqreq
         mounted(){
 
-            // if(this.role == '0004'|| this.role == '0005' ){
+            if(this.role == '0004'|| this.role == '0005' ){
                 this.searchFaq( );//리스트 바인딩
-            // }
+            }
         }
 
         //조회
@@ -147,13 +155,13 @@
 
             let searchData: any = {};
 
-            searchData['viewType'] = 'ALL';
+            searchData['viewType'] = 'ADM';
             //검색
             searchData['searchWord'] = this.searchKey;
 
             // 페이징요청건
-            searchData['currentPage'] = this.listItem.paging.currentPage;
-            searchData['perPage'] = this.listItem.paging.perPage;
+            searchData['currentPage'] = this.pagingUser.currentPage;
+            searchData['perPage'] = this.pagingUser.perPage;
 
             // api 데이터 호출
             CommonBoardService.getListDatas('faq', null, searchData).then((response) => {
@@ -163,36 +171,38 @@
                         this.listData=result.data;
                      }
 
-                    // this.totalCount = result.totalRecords;
-                    // this.startPage= (result.currentPage -1) * result.perPage;
-                    // this.pageSet(result.from, result.to, result.lastPage, result.perPage, result.totalRecords, result.viewPageSize);
-                    //this.$Progress.finish();
+                    this.totalCount = result.totalRecords;
+                    this.startPage= (result.currentPage -1) * result.perPage;
+                    this.pageSet(result.from, result.to, result.lastPage, result.perPage, result.totalRecords, result.viewPageSize);
+                    this.pageSet(result.from, result.to, result.lastPage, result.perPage, result.totalRecords, result.viewPageSize);
+                    // this.$Progress.finish();
                 }
                 , (error) => {
                     //this.$Progress.finish();
                 }
             ).catch();
         }
-        // pageSet(froms, to, lastPage, perPage, totalRecords, viewPageSize) {
-        //     this.listItem.paging.from = froms;
-        //     this.listItem.paging.to = to;
-        //     this.listItem.paging.lastPage = lastPage;
-        //     this.listItem.paging.perPage = perPage;
-        //     this.listItem.paging.totalRecords = totalRecords;
-        //     this.listItem.paging.viewPageSize = viewPageSize;
-        //     //this.$emit('dataToPaging', this.listItem.paging);
-        //     this.dataEvent(this.listItem.paging);
-        // }
-        // pagingEvent(page) {
-        //     this.searchFaq();
-        // }
-        // dataEvent(es){
-        //     //데이터 로딩된후 이벤트를 받으면  변경된 페이지 정보를 전달해준다.
-        //     if(es.lastPage > 0){
-        //         this.$children['0'].defaultPaging(es)
-        //         this.$children['0'].arrayPaging(es);
-        //     }
-        // }
+        pageSet(froms, to, lastPage, perPage, totalRecords, viewPageSize) {
+            this.pagingUser.from = froms;
+            this.pagingUser.to = to;
+            this.pagingUser.lastPage = lastPage;
+            this.pagingUser.perPage = perPage;
+            this.pagingUser.totalRecords = totalRecords;
+            this.pagingUser.viewPageSize = viewPageSize;
+            // this.$emit('dataToPaging', this.pagingUser);
+            this.dataEvent(this.pagingUser);
+        }
+        pagingEvents(page) {
+            this.searchFaq();
+        }
+        dataEvent(es){
+            //데이터 로딩된후 이벤트를 받으면  변경된 페이지 정보를 전달해준다.
+            if(es.lastPage > 0){
+                console.log(this.$children['0']);
+                this.$children['0'].defaultPaging(es)
+                this.$children['0'].arrayPaging(es);
+            }
+        }
 
         /**
          * 검색어 초기화 및 리스트 전체목록
@@ -201,9 +211,6 @@
             this.searchKey="";
             this.searchFaq();
         }
-
-
-
         /**
          * 날짜 포맷 변경
          * @param date
@@ -230,13 +237,6 @@
         }
 
         /**
-         * 상세보기
-         */
-        toDetail(seq){
-            this.$router.push({name:'regFnq', params:{seq:seq}})
-        }
-
-        /**
          * 아코디언메뉴 제어
          * @param index
          */
@@ -253,8 +253,6 @@
                 this.arrow="";
             }
         }
-
-
 
         // 뷰페이지 클릭이벤트 받아서 여는곳
         listViewEvent(row){
--- src/components/contents/fnq/RegFnq.vue
+++ src/components/contents/fnq/RegFnq.vue
@@ -25,10 +25,10 @@
                     <tr>
                         <th scope="row">구분</th>
                         <td>
-                            <select id="" name="" class="select form_notice">
-                                <option>사용자</option>
-                                <option>관리자</option>
-                                <option>공통</option>
+                            <select id="" name="" class="select form_notice" v-model="viewType">
+                                <option value="ALL">공통</option>
+                                <option value="USR">사용자</option>
+                                <option value="ADM">관리자</option>
                             </select>
                         </td>
                     </tr>
@@ -77,10 +77,15 @@
         content : string ="";
 
 
-        mounted(){
-            this.seq = this.$route.params.seq;
+        objectKey : any = "";
 
-            if(this.seq == null || this.seq == 'undefinded') {
+
+        mounted(){
+            // this.seq = this.$route.params.seq;
+            this.objectKey = this.$route.params.objectKey;
+            console.log(this.$route.params.objectKey);
+
+            if(this.objectKey == null || this.objectKey == 'undefinded') {
                 this.div_str="등록";
             }else {
                 this.div_str="수정";
@@ -92,19 +97,28 @@
          * 상세정보 호출
          */
         getFnqDetail() {
-            // api 데이터 호출
-            CommonBoardService.getListDatas('faq', this.seq, null).then((response) => {
-                    let result: any = response.data;
 
-                    if (result !=null) {
-                        this.title = result.title;
-                        this.viewType = result.viewType;
-                        this.content = result.content;
+            if(!this.objectKey){
+                alert('접근할수 없습니다')
+                this.$router.push({name:'fnqList'});
+            }else {
+                this.seq = this.objectKey.seq; //가맹점 번호
+
+
+                // api 데이터 호출
+                CommonBoardService.getListDatas('faq', this.seq, null).then((response) => {
+                        let result: any = response.data;
+
+                        if (result != null) {
+                            this.title = result.title;
+                            this.viewType = result.viewType;
+                            this.content = result.content;
+                        }
                     }
-                }
-                , (error) => {
-                    //this.$Progress.finish();
-                }   ).catch();
+                    , (error) => {
+                        //this.$Progress.finish();
+                    }).catch();
+            }
 
         }
 
@@ -116,10 +130,71 @@
         }
 
         /**
-         * 등록
+         * 등록, 수정
          */
         regFnq(){
 
+            this.validationChk();
+            let reqData ={};
+
+            reqData['title'] = this.title;
+            reqData['viewType'] = this.viewType;
+            // reqData['regRole'] = this.regRole;
+            reqData['content'] = this.content;
+
+            if(this.objectKey != null || this.objectKey !=undefined){ //수정일때
+                // api 데이터 호출
+                CommonBoardService.updateListData('faq', this.objectKey, reqData).then((response) => {
+                        if (response.status.toString() == '200') { //메일 전송 완료
+                            alert("수정되었습니다.");
+                            console.log('수정되었습니다');
+                            console.log(response);
+                            // 현금영수증 발급 완료 화면 이동
+                            // this.$router.push({ name:'faq' }) // 라우터 주소를 넣어줘야 히스토리모드 인식
+                        } else { //메일 전송 실패
+                            console.log('수정실패');
+                            console.log(response);
+                        }
+                    }
+                    , (error) => {
+                        //this.$Progress.finish();
+                        console.log(error);
+                    }
+                ).catch();
+
+            }else{//등록일때
+                CommonBoardService.postListDatas('faq', this.objectKey, reqData).then((response) => {
+                        if (response.status.toString() == '200') { //메일 전송 완료
+                            alert("등록되었습니다.");
+                            console.log('등록되었습니다');
+                            console.log(response);
+                            // 현금영수증 발급 완료 화면 이동
+                            // this.$router.push({ name:'faq' }) // 라우터 주소를 넣어줘야 히스토리모드 인식
+                        } else { //메일 전송 실패
+                            console.log('등록실패');
+                            console.log(response);
+                        }
+                    }
+                    , (error) => {
+                        //this.$Progress.finish();
+                        console.log(error);
+                    }
+                ).catch();
+            }
+
+        }
+
+        /**
+         * 유효성체크
+         */
+        validationChk(){
+            if(this.title == null || this.title==""){
+                alert("제목을 입력하세요");
+                return false;
+            }else if(this.content ==null || this.content ==""){
+                alert("내용을 입력하세요");
+                return false;
+            }
         }
 
     }
