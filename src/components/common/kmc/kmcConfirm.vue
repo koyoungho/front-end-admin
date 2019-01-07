fbc02456db136f2b702a332fa080e41320e5b5f4
kimc <chkkim22@neonexsoft.com>
2019-01-07 21:19:01 +0900

    화면 수정

--- src/components/common/kmc/kmcConfirm.vue
+++ src/components/common/kmc/kmcConfirm.vue
@@ -98,7 +98,7 @@
             CommonBoardService.getListData('','kmc/cert','')
                 .then((response) => {
                         this.tr_cert = response.data.cert;
-                        this.tr_url = 'http://211.39.150.112:8080/#/searchIdResult';
+                        this.tr_url = 'http://211.39.150.112:8888/#/searchIdResult';
                         this.sendCerticication();
                     }
                     , error => {
