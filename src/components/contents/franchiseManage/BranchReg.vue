a1a4fe9b8c94393ab016a406e3c19d26d109140b
kimc <chkkim22@neonexsoft.com>
2019-01-08 15:46:42 +0900

    화면 수정

--- src/components/contents/franchiseManage/BranchReg.vue
+++ src/components/contents/franchiseManage/BranchReg.vue
@@ -812,6 +812,9 @@
                         this.saupIdYn = 'Y';
                         //사업자등록번호 유효성 체크에 이상이 없으면 기 등록된 사업장등록번호인지 한번 더 체크
 //                        this.chkSaupNoAlr(no);
+                        if(saupmsg != null){
+                            saupmsg.innerHTML = "사용가능한 사업자등록번호입니다."; //화면에 메시지 보이기
+                        }
                     } else {
                         this.saupIdYn = ''; //개발 반영시 ''로 바꾸어야 함
                         if(saupmsg != null){
