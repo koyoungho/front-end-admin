ec1b211624992fb427b5cf8193dac76dc8b0871f
ko <ignation@neonexsoft.com>
2019-01-08 15:59:16 +0900

    증감현황 완료

--- src/components/contents/mnStatistics/GajumLineChart.vue
+++ src/components/contents/mnStatistics/GajumLineChart.vue
@@ -14,7 +14,7 @@
             <!--</div>-->
             <div class="col" style ="width:100%">
                 <div class="chart_box">
-                    <vcha :data="chartDataJoin" :settings="ChartSettingsJoin" height="280px"></vcha>
+                    <ve-line :data="chartDataJoin" :settings="ChartSettingsJoin" height="280px"></ve-line>
                 </div>
             </div>
         </div>
@@ -24,17 +24,18 @@
         <div class="grid_chart_box col02">
             <div class="col">
                 <ul class="chart_list">
-                    <li class="on"><a href="#">전체</a></li>
-                    <li><a href="#">KT 가맹점 발행</a></li>
-                    <li><a href="#">롯데 가맹점 발행</a></li>
-                    <li><a href="#">일반 발행</a></li>
-                    <li><a href="#">웹 발행</a></li>
+                    <li class="on"><a @click="dataCheck('all')">전체</a></li>
+                    <li><a  @click="dataCheck('ktAprv')">KT 승인</a></li>
+                    <li><a  @click="dataCheck('ktCancel')">KT 취소</a></li>
+                    <li><a  @click="dataCheck('ldccAprv')">롯데 승인</a></li>
+                    <li><a  @click="dataCheck('ldccCancel')">롯데 취소</a></li>
                 </ul>
             </div>
             <div class="col">
                 <div class="chart_box">
                     <!--<img src="../../../assets/images/img_chart01.png" alt="" style="margin:30px 50px 0 50px">-->
-                    <ve-line :data="chartDataCash" :settings="ChartSettingsCash" height="280px"></ve-line>
+                    <ve-line v-bind:data="chartDataCash" v-bind:settings="ChartSettingsCash" height="280px"></ve-line>
+
                 </div>
             </div>
         </div>
@@ -68,6 +69,34 @@
         nowDate : any = new Date();
         dateArray : any = [];
         dateArray2 : any = [];
+        chart1 : boolean = false;
+        OringinChart : any = "";
+
+        ChartSettingsJoin :any = {
+            labelMap: {
+                ktAprv: 'KT가입',
+                ktCancel: 'KT해지',
+                ldccAprv: '롯데가입',
+                ldccCancel: '롯데해지',
+            }
+        };
+        chartDataJoin :any = {
+            columns: ['date', 'ktAprv', 'ktCancel', 'ldccAprv', 'ldccCancel'],
+            rows: [
+            ]}
+        //     //현금영수증
+        ChartSettingsCash : any  ={
+            labelMap: {
+                ktAprv: 'KT승인',
+                ktCancel: 'KT취소',
+                ldccAprv: '롯데승인',
+                ldccCancel: '롯데취소',
+            }
+        }
+        chartDataCash : any = {
+            columns: ['date', 'ktAprv', 'ktCancel', 'ldccAprv', 'ldccCancel'],
+            rows: [
+            ]}
 
         @Watch('searchStartDate') onChange(){
             this.newDateStart = this.searchStartDate
@@ -76,53 +105,20 @@
             this.newDateEnd = this.searchEndDate
         }
 
+        dataCheck(data){
+            if (data =='all'){
+                this.chartDataCash.columns = this.OringinChart
+            }else{
+                this.chartDataCash.columns= ['date', data]
+            }
+        }
+
         formatDates(date) {
             let formattedDates = ''
             formattedDates = format(date, 'YYYYMM')
             return formattedDates
         }
 
-        //가맹점현황
-        ChartSettingsJoin : any  ={
-            labelMap: {
-                ktAprv: 'KT가입',
-                ktCancel: 'KT해지',
-                ldccAprv: '롯데가입',
-                ldccCancel: '롯데해지',
-            },
-
-        }
-        chartDataJoin : any = {
-            columns: ['date', 'ktAprv', 'ktCancel', 'ldccAprv', 'ldccCancel'],
-            rows: [
-
-            ]}
-
-            //현금영수증
-        ChartSettingsCash : any  ={
-            labelMap: {
-                cost: 'KT 가맹점 발행',
-                profit: '롯데 가맹점 발행',
-                growthRate: '일반발행',
-                people: '웹발행',
-            }
-        }
-        chartDataCash : any = {
-            columns: ['date', 'cost', 'profit', 'growthRate', 'people'],
-            rows: [
-                { 'cost': 152, 'date': '01/01', 'profit': 152, 'growthRate': 0.12, 'people': 100 },
-                { 'cost': 122, 'date': '01/02', 'profit': 153, 'growthRate': 0.345, 'people': 100 },
-                { 'cost': 212, 'date': '01/03', 'profit': 153, 'growthRate': 0.7, 'people': 100 },
-                { 'cost': 412, 'date': '01/04', 'profit': 123, 'growthRate': 0.31, 'people': 100 },
-                { 'cost': 312, 'date': '01/05', 'profit': 123, 'growthRate': 0.12, 'people': 100 },
-                { 'cost': 712, 'date': '01/06', 'profit': 152, 'growthRate': 0.65, 'people': 100 },
-                { 'cost': 152, 'date': '01/07', 'profit': 152, 'growthRate': 0.12, 'people': 100 },
-                { 'cost': 122, 'date': '01/08', 'profit': 153, 'growthRate': 0.345, 'people': 100 },
-                { 'cost': 212, 'date': '01/09', 'profit': 153, 'growthRate': 0.7, 'people': 100 },
-                { 'cost': 412, 'date': '01/10', 'profit': 123, 'growthRate': 0.31, 'people': 100 },
-                { 'cost': 312, 'date': '01/11', 'profit': 123, 'growthRate': 0.12, 'people': 100 },
-                { 'cost': 712, 'date': '01/12', 'profit': 152, 'growthRate': 0.65, 'people': 100 }
-            ]}
 
         dateFormats(date) {
             let dates = '';
@@ -131,16 +127,16 @@
         }
 
         created(){
-            this.gajumStatistics()
         }
 
         mounted(){
 
         }
 
-        gajumStatistics(){
-            CommonBoardService.getListDatas('statistics','gajum',{responseType:'CHART',searchStartDate: this.newDateStart , searchEndDate: this.newDateEnd}).then(result=>{
-                if(result.status==200){
+        gajumStatisticsChart(date1,date2){
+            // this.chart1=true;
+            CommonBoardService.getListDatas('statistics','gajum',{responseType:'CHART',searchStartDate: date1 , searchEndDate: date2 }).then(result=>{
+                if(result.status==200) {
                     this.gajumList = result.data
                     this.chartDataJoin.rows=[];
                     result.data.filter(e=>{
@@ -152,15 +148,16 @@
             })
         }
 
-        receuptStatistics(){
-            CommonBoardService.getListDatas('statistics','receipt',{responseType:'CHART',searchStartDate: this.newDateStart , searchEndDate: this.newDateEnd}).then(result=>{
+        receuptStatisticsChart(date1,date2){
+            CommonBoardService.getListDatas('statistics','receipt',{responseType:'CHART',searchStartDate: date1 , searchEndDate: date2}).then(result=>{
                 if(result.status==200){
                     console.log(result)
                     this.receiptList = result.data
-                    // this.chartDataCash = {
-                    //     column: [ObjectColumn],
-                    //     row : [ObjectRowList]
-                    // }
+                    this.chartDataCash.rows=[];
+                    result.data.filter(e=>{
+                        this.chartDataCash.rows.push(e)
+                    })
+                    this.OringinChart = this.chartDataCash.columns
                 }
             }).catch(e=>{
             })
--- src/main.ts
+++ src/main.ts
@@ -52,7 +52,6 @@ Vue.component('tinymce', tinymce)
  * 차트
  */
 import VCharts from 'v-charts'
-Vue.component('vcha' , VCharts)
 Vue.use(VCharts);
 
 // import VueDragscroll from 'vue-dragscroll'
