1d13126d493af3086671e690af3a4065e81894ac
ko <ignation@neonexsoft.com>
2019-01-13 17:27:39 +0900

    발급조회 수정완료

--- src/components/contents/notice/NoticeDetl.vue
+++ src/components/contents/notice/NoticeDetl.vue
@@ -142,23 +142,70 @@
          * 파일다운로드
          * @param fileNm
          */
-        download(fileNm, fileOrigin){
-            //파일 다운로드
+        // download(fileNm, fileOrigin){
+        //     //파일 다운로드
+        //     axios({
+        //         url: environment.apiUrl +"/file/"+fileNm,
+        //         method: 'GET',
+        //         responseType: 'blob', // important
+        //         headers: {"x-auth-token": sessionStorage.accessToken}
+        //     }).then((response) => {
+        //         console.log(response);
+        //         const url = window.URL.createObjectURL(new Blob([response.data],{type: 'application/octet-stream'}));
+        //         console.log('url',url)
+        //         const link = document.createElement('a');
+        //
+        //         window.navigator.msSaveBlob(url,fileNm)
+        //
+        //         link.href = url;
+        //         link.target = "_blank";
+        //         link.download = fileOrigin;
+        //         link.click();
+        //         // link.setAttribute('download', fileOrigin); //or any other extension
+        //         // document.body.appendChild(link);
+        //         // link.click();
+        //         // document.body.removeChild(link);
+        //         // window.URL.revokeObjectURL(url);
+        //     });
+        // }
+
+        //fileNm, fileOrigin
+        download(fileNm, fileOrigin) {
             axios({
                 url: environment.apiUrl +"/file/"+fileNm,
                 method: 'GET',
                 responseType: 'blob', // important
                 headers: {"x-auth-token": sessionStorage.accessToken}
             }).then((response) => {
-                console.log(response);
-                const url = window.URL.createObjectURL(new Blob([response.data]));
-                console.log('url',url)
-                const link = document.createElement('a');
-                link.href = url;
-                link.setAttribute('download', fileOrigin); //or any other extension
-                document.body.appendChild(link);
-                link.click();
-            });
+                console.log(response)
+                // It is necessary to create a new blob object with mime-type explicitly set
+                // otherwise only Chrome works like it should
+                var newBlob = new Blob([response.data],{type: 'application/pdf'})
+
+                // IE doesn't allow using a blob object directly as link href
+                // instead it is necessary to use msSaveOrOpenBlob
+                if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+                    window.navigator.msSaveOrOpenBlob(newBlob)
+                    return
+                }
+
+                // For other browsers:
+                // Create a link pointing to the ObjectURL containing the blob.
+                const data = window.URL.createObjectURL(newBlob)
+                var link = document.createElement('a')
+                link.href = data
+                link.download = fileOrigin + '.pdf'
+
+                link.click()
+                setTimeout(function () {
+                    // For Firefox it is necessary to delay revoking the ObjectURL
+                    window.URL.revokeObjectURL(data)
+                }, 100)
+
+
+
+            })
+
         }
 
     }
