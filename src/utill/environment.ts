dadb842be48f9c9f0656ab3a746b3a50ca71c51f
kimc <chkkim22@neonexsoft.com>
2019-01-17 23:20:18 +0900

    메일 발송

--- src/utill/environment.ts
+++ src/utill/environment.ts
@@ -2,9 +2,10 @@
 export const environment = {
     production: false,
     apiUrl : "http://211.39.150.96:8888/api", //서비스 API 실서버.
-    // apiUrl : "http://211.39.150.112:80/admin/api", //서비스 API 개발서버.
+    // apiUrl : "http://211.39.150.112:80/admin/api", //서비스 API 개발서버.siteUrl
     imgApiUrl : "http://211.39.150.96:8888",   // 서비스 메일이미지 실서버
     // imgApiUrl : "http://211.39.150.112",    // 서비스 메일이미지 개발서버
+    userUrl : "http://211.39.150.96",   // 온라인 가맹점 사용자 URL
 
     addressKey : "U01TX0FVVEgyMDE4MTEwNzE2MTEyMTEwODI4NTU=", // 주소 오픈API 키
     addressApiUrl : "http://www.juso.go.kr/addrlink/addrLinkApi.do", // 주소 URL
