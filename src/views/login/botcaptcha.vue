069e484443925861b4fd5ebf6462d0d02975df32
ko <ignation@neonexsoft.com>
2019-01-17 13:48:23 +0900

    api url 변경 한곳으로 취합

--- src/views/login/botcaptcha.vue
+++ src/views/login/botcaptcha.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="captcha_box">
-    <img src="http://211.39.150.112/api/captcha" alt="캡차 이미지" title="클릭시 새로고침" @click="imgRefresh()"/><br>
+    <img :src="environment.apiUrl+'/captcha'" alt="캡차 이미지" title="클릭시 새로고침" @click="imgRefresh()"/><br>
     <input type="text" id="captcha" name="captcha" autocomplete="off" title="보안문자를 입력해 주세요"  required/>
   </div>
 </template>
 
 <script>
+  import {environment} from '/src/utill/environment'
+
   export default {
     name: 'captcha',
       methods: {
           //이미지 새로고침
           imgRefresh(){
-              this.$el.children[0].src = "http://211.39.150.96:8888/api/captcha?ran="+ Math.random();
+              this.$el.children[0].src = +environment.apiUrl+/captcha?ran="+ Math.random();
           }
       }
   };
