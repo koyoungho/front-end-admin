8672496ec0672f594f409a3804322dcc6274d1eb
ko <ignation@neonexsoft.com>
2019-01-17 22:30:36 +0900

    수정

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -194,10 +194,11 @@
         onSubmits(id, password) {
 
             if (id == '') {
-                alert('아이디를 입력하세요.');
+                Vue.swal({text: '아이디를 입력하세요'});
                 return;
             } else if (password == '') {
                 alert('비밀번호를 입력하세요.');
+                Vue.swal({text: '비밀번호를 입력하세요'});
                 return;
             }
 
