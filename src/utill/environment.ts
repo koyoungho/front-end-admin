87e64e29ef27e3ee08203500a1aed1b4a5f773f8
kimc <chkkim22@neonexsoft.com>
2019-01-21 13:20:04 +0900

    url 수정

--- src/utill/environment.ts
+++ src/utill/environment.ts
@@ -1,11 +1,13 @@
 
 export const environment = {
     production: false,
-    apiUrl : "https://admin.hellocash.co.kr/api", //서비스 API 실서버.
-    // apiUrl : "http://211.39.150.112:80/admin/api", //서비스 API 개발서버.siteUrl
-    imgApiUrl : "https://admin.hellocash.co.kr",   // 서비스 메일이미지 실서버
-    // imgApiUrl : "http://211.39.150.112",    // 서비스 메일이미지 개발서버
-    userUrl : "https://www.hellocash.co.kr",   // 온라인 가맹점 사용자 URL
+    //apiUrl : "https://admin.hellocash.co.kr/api", //서비스 API 실서버.
+    //imgApiUrl : "https://admin.hellocash.co.kr",   // 서비스 메일이미지 실서버
+    //userUrl : "https://www.hellocash.co.kr",   // 온라인 가맹점 사용자 URL
+
+    apiUrl : "http://211.39.150.112:8888/admin/api", //서비스 API 개발서버.siteUrl
+    imgApiUrl : "http://211.39.150.112:8888",    // 서비스 메일이미지 개발서버
+    userUrl : "http://211.39.150.112:8888",    // 서비스 메일이미지 개발서버
 
     addressKey : "U01TX0FVVEgyMDE4MTEwNzE2MTEyMTEwODI4NTU=", // 주소 오픈API 키
     addressApiUrl : "https://www.juso.go.kr/addrlink/addrLinkApi.do", // 주소 URL
